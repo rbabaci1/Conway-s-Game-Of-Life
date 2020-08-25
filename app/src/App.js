3c6f155c9039f25c268a0dbce41ea9bfd92b6487
rabah babaci <4rabah@gmail.com>
2020-08-25 07:25:53 -0700

    define the full grid state

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 
-import Grid from "./components/Grid";
+import Grid from "./components/Grid/index";
 import "./App.scss";
 
 function App() {
+  let speed = 100;
+  let rows = 30;
+  let columns = 50;
+
   const [generation, setGeneration] = useState(0);
+  const [fullGrid, setFullGrid] = useState(
+    Array(rows)
+      .fill()
+      .map(() => Array(columns).fill(false))
+  );
 
   return (
     <div className="App">
       <h1>Game Of Life!</h1>
       <h2>Generations: {generation}</h2>
-      <Grid />
+
+      <Grid fullGrid={fullGrid} rows={rows} columns={columns} />
     </div>
   );
 }
---
+++ app/src/components/Grid/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./grid.scss";
+
+export default function Grid() {
+  return <div className="grid-container">Grid</div>;
+}
