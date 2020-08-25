8e65cd38059339ceb233f31b5b9b8a2477cc3dff
rabah babaci <4rabah@gmail.com>
2020-08-25 09:24:45 -0700

    add comments to the play method

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import Grid from "./components/Grid/index";
 import "./App.scss";
 
 function App() {
-  let speed = 100;
+  let speed = 1000;
   let rows = 30;
   let columns = 50;
 
@@ -34,8 +34,52 @@ function App() {
     setFullGrid(gridCopy);
   };
 
+  const play = () => {
+    let gridCopy = [...fullGrid];
+    /*
+      1- For i to be a valid index, must be bigger than 0 smaller than rows - 1
+      2- For j to be a valid index, must be bigger than 0 smaller than columns - 1
+    */
+
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        let counter = 0;
+        // if top neighbor is alive and i is valid
+        if (i > 0) if (fullGrid[i - 1][j]) counter++;
+        // if top left neighbor is alive and i & j are valid
+        if (i > 0 && j > 0) if (fullGrid[i - 1][j - 1]) counter++;
+        // if top right neighbor is alive and i & j are valid
+        if (i > 0 && j < columns - 1) if (fullGrid[i - 1][j + 1]) counter++;
+        // if right neighbor is alive and and i & j are valid
+        if (j < columns - 1) if (fullGrid[i][j + 1]) counter++;
+        // if left neighbor is alive and and  j is valid
+        if (j > 0) if (fullGrid[i][j - 1]) counter++;
+        // if bottom neighbor is alive and i is valid
+        if (i < rows - 1) if (fullGrid[i + 1][j]) counter++;
+        // if bottom left neighbor is alive and i & j are valid
+        if (i < rows - 1 && j > 0) if (fullGrid[i + 1][j - 1]) counter++;
+        // if bottom right neighbor is alive and i & j are valid
+        if (i < rows - 1 && columns - 1) if (fullGrid[i + 1][j + 1]) counter++;
+
+        // if a cell is alive and has less than 2 or more than 3 live neighbors, it will die
+        if (fullGrid[i][j] && (counter < 2 || counter > 3))
+          gridCopy[i][j] = false;
+        // if a cell is dead and has 3 live neighbors, it will be born
+        if (!fullGrid[i][j] && counter === 3) gridCopy[i][j] = true;
+      }
+    }
+
+    setFullGrid(gridCopy);
+  };
+
+  const playGame = () => {
+    let intervalId = setInterval(play, speed);
+    clearInterval(intervalId);
+  };
+
   useEffect(() => {
     generateCells();
+    // playGame();
   }, []);
 
   return (
