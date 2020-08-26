56011f656063459c0d224e57d0f8f538a486761e
rabah babaci <4rabah@gmail.com>
2020-08-26 15:14:36 -0700

    add an info icon to display game info

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,7 @@ import Grid from "./components/Grid/index";
 import Commands from "./components/Commands";
 import setInitialState from "./initialState";
 import helpers from "./helpers";
+import infoIcon from "./images/infoIcon.svg";
 import "./index.scss";
 
 class App extends Component {
@@ -113,7 +114,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Game Of Life!</h1>
+        <h1>Conway's Game Of Life</h1>
+        <img src={infoIcon} className="info-icon" />
         <h2>Generations: {this.state.generation}</h2>
 
         <Grid
