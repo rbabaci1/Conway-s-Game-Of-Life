02bb7b5fc7bcdfd0f5a553348e53a648f516bade
rabah babaci <4rabah@gmail.com>
2020-08-26 12:28:10 -0700

    refactor the Grid component

--- app/src/components/Grid/index.js
+++ app/src/components/Grid/index.js
@@ -4,28 +4,20 @@ import Cell from "../Cell";
 import "./grid.scss";
 
 export default function Grid({ grid, columns, selectCell }) {
-  let cellClass = "";
-
-  let rowsList = grid.map((row, rowIndex) =>
-    row.map((col, colIndex) => {
-      cellClass = grid[rowIndex][colIndex] ? "cell alive" : "cell dead";
-
-      return (
-        <Cell
-          cellClass={cellClass}
-          cellId={`${rowIndex}_${colIndex}`}
-          key={`${rowIndex}_${colIndex}`}
-          row={rowIndex}
-          col={colIndex}
-          selectCell={selectCell}
-        />
-      );
-    })
-  );
-
   return (
     <div className="grid-container" style={{ width: columns * 16 }}>
-      {rowsList}
+      {grid.map((row, rowIndex) =>
+        row.map((col, colIndex) => (
+          <Cell
+            cellClass={grid[rowIndex][colIndex] ? "cell alive" : "cell dead"}
+            cellId={`${rowIndex}_${colIndex}`}
+            key={`${rowIndex}_${colIndex}`}
+            row={rowIndex}
+            col={colIndex}
+            selectCell={selectCell}
+          />
+        ))
+      )}
     </div>
   );
 }
