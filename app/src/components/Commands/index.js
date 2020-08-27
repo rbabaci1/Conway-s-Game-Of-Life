2040304238a9665f1695b60ab745080910cb3027
rabah babaci <4rabah@gmail.com>
2020-08-27 09:29:35 -0700

    install material UI icons

--- app/src/components/Commands/index.js
+++ app/src/components/Commands/index.js
@@ -5,6 +5,7 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
 import Button from "../Button";
+import SpeedSlider from "../SpeedSlider";
 import "./commands.scss";
 
 export default function Commands(commands) {
